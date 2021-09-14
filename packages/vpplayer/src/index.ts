21ae2921c5939a14d0f13410fafa6be65e5713df
utauuAva <allenwill211@gmail.cm>
2021-09-14 15:42:56 +0800

    style: re build

--- packages/vpplayer/src/index.ts
+++ packages/vpplayer/src/index.ts
@@ -2,11 +2,13 @@
  * @Author: Allen OYang
  * @Date: 2021-07-01 16:36:04
  * @Descripttion: 
- * @LastEditTime: 2021-07-29 19:05:12
+ * @LastEditTime: 2021-09-14 15:40:46
  * @FilePath: /plugin-core/packages/vpplayer/src/index.ts
  */
+
+
 import vpplayer from './core'
-import  './skin';
+import './skin';
 import './controls';
 
 
